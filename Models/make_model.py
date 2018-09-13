0254edb1b3b954cc4b2066ffecbd539ff1141697
BrianRod <brod4910@gmail.com>
2018-09-13 09:40:59 -0600

    Fixes bug that skips the Linear Layer with Tanh activation

--- Models/make_model.py
+++ Models/make_model.py
@@ -71,7 +71,7 @@ def make_classifier_layers(layout):
                 layers += [nn.Linear(layer[1], layer[2]), nn.BatchNorm1d(layer[2]), nn.SELU(inplace= True)]
             elif layer[3] == 'LeakyReLU':
                 layers += [nn.Linear(layer[1], layer[2]), nn.BatchNorm1d(layer[2]), nn.LeakyReLU(inplace= True)]
-            elif layer[0] == 'Tanh':
+            elif layer[3] == 'Tanh':
                 layers += [nn.Linear(layer[1], layer[2]), nn.Tanh()]
         elif layer[0] == 'D':
             layers += [nn.Dropout(layer[1])]
