84ce667d5b448aaaa8aee13d381b56a7358b73f2
BrianRod <brod4910@gmail.com>
2018-09-23 00:36:07 -0600

    k_folds imports

--- k_fold.py
+++ k_fold.py
@@ -1,4 +1,12 @@
-import numpy as numpy
+from __future__ import print_function, division
+import os
+import torch
+import pandas as pd
+from skimage import io, transform
+import numpy as np
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms, utils
+from PIL import Image
 from sklearn.model_selection import KFold
 
 [self.inputs = np.load(os.path.join(root_dir, input_file))
@@ -9,5 +17,6 @@ self.root_dir = root_dir]
 kfold = KFold(4, True, 11)
 
 for train, test in kfold.split(new_data):
-	print(train, test)
-    # print('train: %s, test: %s' % (new_data[train], new_data[test]))
+    print('train: %s, test: %s' % (new_data[train], new_data[test]))
+	
+print(train, test)
